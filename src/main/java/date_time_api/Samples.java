a5ad37d3fe6627873ff44c10dec35603d7fc2901
ounihamza100 <hamza.ellouni@gmail.com>
2019-10-09 17:37:42 +0100

    code

--- src/main/java/date_time_api/Samples.java
+++ src/main/java/date_time_api/Samples.java
@@ -1,15 +1,18 @@
 package date_time_api;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
+import java.util.Set;
 
 /**
  * @author Hamza Ouni
  */
 public class Samples {
+
+
+    //==================== The LocalDate represents a date in ISO format (yyyy-MM-dd) without time =========================
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     /**
      *
@@ -57,6 +60,84 @@ public class Samples {
         LocalDate firstDayOfMonth = LocalDate.parse("2016-06-12")
                 .with(TemporalAdjusters.firstDayOfMonth());
         System.out.println("firstDayOfMonth : "+firstDayOfMonth);
+
+
+        //==================== LocalTime represents time without a date =========================
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        LocalTime now = LocalTime.now();
+
+        // create a LocalTime representing 06:30 AM by parsing a string representation
+        LocalTime sixThirty = LocalTime.parse("06:30");
+        System.out.println("sixThirty : "+sixThirty);
+
+        LocalTime sixThirty_using_factory_method = LocalTime.of(6, 30);
+
+        // creates a LocalTime by parsing a string and adds an hour to it by using the “plus” API.
+        // The result would be LocalTime representing 07:30 AM
+        LocalTime sevenThirty = LocalTime.parse("06:30").plus(1, ChronoUnit.HOURS);
+
+        //
+        int six = LocalTime.parse("06:30").getHour();
+
+        //check if a specific time is before or after another specific time
+        boolean isbefore = LocalTime.parse("06:30").isBefore(LocalTime.parse("07:30"));
+
+        //Max , Min and Noon
+        LocalTime maxTime = LocalTime.MAX;
+        LocalTime minTime = LocalTime.MIN;
+        LocalTime noonTime = LocalTime.NOON;
+
+        System.out.println("maxTime :"+maxTime);
+        System.out.println("minTime :"+minTime);
+        System.out.println("noonTime :"+noonTime);
+
+        //==================== The LocalDateTime is used to represent a combination of date and time =========================
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime.of(2015, Month.FEBRUARY, 20, 06, 30);
+        LocalDateTime.parse("2015-02-20T06:30:00");
+        localDateTime.plusDays(1);
+        localDateTime.minusHours(2);
+        localDateTime.getMonth();
+
+        //==================== Using ZonedDateTime API =========================
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        //Java 8 provides ZonedDateTime when we need to deal with time zone specific date and time.
+        // The ZoneId is an identifier used to represent different zones.
+        // There are about 40 different time zones and the ZoneId are used to represent them as follows.
+
+        //In this code snippet we create a Zone for Paris
+        ZoneId zoneId = ZoneId.of("Europe/Paris");
+        System.out.println("ZoneId : "+zoneId);
+
+        //A set of all zone ids
+        Set<String> allZoneIds = ZoneId.getAvailableZoneIds();
+
+
+        //LocalDateTime can be converted to a specific zone:
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, zoneId);
+
+        //====================  Using Period and Duration =======================================
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        //The Period class represents a quantity of time in terms of years, months and days and
+        // the Duration class represents a quantity of time in terms of seconds and nano seconds.
+
+        //The Period class is widely used to modify values of given a date or to obtain the difference between two dates
+        LocalDate initialDate = LocalDate.parse("2007-05-10");
+        LocalDate finalDate = initialDate.plus(Period.ofDays(5));
+        int five = Period.between(finalDate, initialDate).getDays();
+        long fivee = ChronoUnit.DAYS.between(finalDate , initialDate);
+
+
+
+
+
+
+
+
+
 
 
 
